3de1a49ab0f68c8604612452c8c03d82871cd601
LysenkovIlya-Hexlet <lysenkov_iv21@mail.ru>
2025-06-28 20:14:57 +0300

    Убрана ошибка линтера E501

--- tests/test_formaters.py
+++ tests/test_formaters.py
@@ -3,12 +3,20 @@ from tests import build_fixture_path
 import pytest
 
 
-@pytest.mark.parametrize("custom_format,expected,file1,file2",
-                         [("JSON", "json_right_result", "file1.json", "file2.json"),
-                          ("PLAIN", "plain_right_result", "file1.json", "file2.json"),
-                          ("STYLISH", "stylish_right_result", "file1.json", "file2.json")])
+@pytest.mark.parametrize(
+    "custom_format,expected,file1,file2",
+    [
+        ("JSON", "json_right_result", "file1.json", "file2.json"),
+        ("PLAIN", "plain_right_result", "file1.json", "file2.json"),
+        ("STYLISH", "stylish_right_result", "file1.json", "file2.json")
+    ]
+)
 def test_plain_and_json(custom_format, expected, file1, file2):
     with open(build_fixture_path(expected)) as expected_file:
         expected_content = expected_file.read()
-        assert gendiff.generate_diff(build_fixture_path(file1),
-                                     build_fixture_path(file2), custom_format) == expected_content
+        actual = gendiff.generate_diff(
+            build_fixture_path(file1),
+            build_fixture_path(file2),
+            custom_format
+        )
+        assert actual == expected_content
